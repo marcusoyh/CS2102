59eedffd81bc840118f468607802fb9f8719d6e4
Celesse Tay <celessetay@gmail.com>
2020-04-20 17:33:03 +0800

    update

--- App/routes/summaryInfo.js
+++ App/routes/summaryInfo.js
@@ -7,39 +7,72 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL //we need to edit the .env individually, and put it in .gitignore
 });
 
-var selectQuery = ' SELECT COALESCE (v1.month,v2.month) as month, v1.totalorders,v1.totalcost,v2.foodname from viewOne v1 full join viewTwo v2 on v1.month=v2.month order by v1.month';
+var selectQuery = 'SELECT month, totalorders, totalcost from viewOne order by month';
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var minutes = ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"];
-/* GET method to generate the tables */
-router.get('/:id', function (req, res, next) {
 
-	var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems group by month)';
-	var viewTwo = ', viewtwo as (SELECT count(*) as numorders, foodname from RestaurantFoodItems where rid = $1 group by foodname order by numorders)';
-	var getQuery = viewOne + viewTwo + selectQuery;
-	//this coalesce is the way to join two tables on a common column, but overlap instead of inner joining!
-	
+// GET
+router.get('/:id', function(req, res, next) {
+	res.render('summarInfo', { title: 'Summary Information' });
+});
 
-	pool.query(getQuery, [rid],(err, data) => {
-		res.render('summaryInfo', { title: 'View Summary Information', months: months, data: data.rows });
+// POST
+router.post('/:id', function(req, res, next) {
+	// Retrieve Information
+	var month  = req.body.month;
+	var year = req.body.year;
+
+	// Construct Specific SQL Query
+	var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
+	var getQuery = viewOne + selectQuery;
+
+	pool.query(getQuery, [id],(err, data) => {
+		res.render('summaryInfo2', { title: 'View Summary Information', months: months, data: data.rows });
 	});
 });
 
 
-// POST method to filter table
-router.post('/', function (req, res, next) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* GET method to generate the tables 
+router.get('/:id', function (req, res, next) {
+	const id = req.params.id;
+	var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
+	var getQuery = viewOne + selectQuery;
+
+	pool.query(getQuery, [id],(err, data) => {
+		res.render('summaryInfo', { title: 'View Summary Information', months: months, data: data.rows });
+	});
+});*/
+
+
+/* POST method to filter table
+router.post('/:id', function (req, res, next) {
 	// Retrieve Information
 	// var dateString = req.body.monthToFilter;
 
-	var postViewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(deliveryfee) as totalcost from orders where month between ' + "'2013-1-1' and " +" '2016-1-1'" + 'group by month)';
-	var postViewTwo = ', viewtwo as (SELECT DATE_TRUNC(' + "'month'" + ',signupdate) AS month, count(uid) as totalsignups from customers where month between ' + "'2013-1-1' and " +" '2016-1-1'" + 'group by month)';
+	var postViewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1, month between ' + "'2013-1-1' and " +" '2016-1-1'" + 'group by month)';
 	
-	var postQuery = postViewOne + postViewTwo + selectQuery;
+	var postQuery = postViewOne +  selectQuery;
 	//date.isValid() is a method that returns true/false, can validate whether legit date was entered
 	pool.query(postQuery, (err, data) => {
-		res.render('fdsmanager/fdsone', { title: 'Summary One, Filtered', months: months, data: data.rows })
+		res.render('summaryInfo', { title: 'Summary Info, Filtered', months: months, data: data.rows })
 	});
 
-});
+});*/
 
 //to query for searching for specific months, we do select * from orders where timeDelivered between '2015-10-10' and '2016-10-10';
 module.exports = router;
--- init.sql
+++ init.sql
@@ -146,6 +146,7 @@ CREATE TABLE Orders(
   commission REAL,
   riderRating INTEGER, 
   deliveryTime TIMESTAMP,
+  totalprice REAL,
   lid INTEGER references Locations not null,
   did INTEGER not null,
   cid INTEGER not null,
@@ -228,4 +229,5 @@ INSERT INTO Orders (oid,orderReview,deliveryFee,timeOrdered,paymentMode,isDelive
 INSERT INTO Orders (oid,orderReview,deliveryFee,timeOrdered,paymentMode,isDelivered,timeRiderDeparts,timeRiderReachesRestaurant,timeRiderLeavesRestaurant,commission,riderRating,deliveryTime,lid,did,cid,rid) VALUES (7,'Alright',2.2,'2020-04-30 11:50:00','cash',false,'2020-04-30 13:40:00', '2020-04-30 14:00:00','2020-04-30 15:00:00',2,5,'2020-04-30 15:30:00',2,2,1,2);
 INSERT INTO Orders (oid,orderReview,deliveryFee,timeOrdered,paymentMode,isDelivered,timeRiderDeparts,timeRiderReachesRestaurant,timeRiderLeavesRestaurant,commission,riderRating,deliveryTime,lid,did,cid,rid) VALUES (8,'Can be better',2,'2014-12-17 12:30:00','cash',false,'2014-12-17 13:00:00', '2014-12-17 13:30:00','2014-12-17 13:40:00',3,4,'2014-12-17 14:10:00',2,2,6,2);
 INSERT INTO Orders (oid,orderReview,deliveryFee,timeOrdered,paymentMode,isDelivered,timeRiderDeparts,timeRiderReachesRestaurant,timeRiderLeavesRestaurant,commission,riderRating,deliveryTime,lid,did,cid,rid) VALUES (9,'Not good',2,'2014-12-17 12:00:00','cash',false,'2014-12-17 13:00:00', '2014-12-17 13:30:00','2014-12-17 13:40:00',2,3,'2014-12-17 14:30:00',2,2,6,2);
- 
+INSERT INTO OrderContainsFoodItems(1,1,"Cheeseburger",2) ;
+INSERT INTO OrderContainsFoodItems(2,1,"Cheeseburger",3);
