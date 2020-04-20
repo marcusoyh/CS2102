30964ba5d57da802b792402d8d5a8d0833fea02b
Celesse Tay <celessetay@gmail.com>
2020-04-20 23:17:19 +0800

    update

--- App/routes/summaryInfo.js
+++ App/routes/summaryInfo.js
@@ -7,27 +7,62 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL //we need to edit the .env individually, and put it in .gitignore
 });
 
-var selectQuery = 'SELECT month, totalorders, totalcost from viewOne order by month';
+var selectQuery = 'SELECT month, totalorders, totalcost from viewOne group by month';
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 // GET
 router.get('/:id', function(req, res, next) {
-	res.render('summarInfo', { title: 'Summary Information' });
+	const id = req.params.id;
+	res.render('summaryInfo', { title: 'Summary Information' });
 });
 
 // POST
 router.post('/:id', function(req, res, next) {
 	// Retrieve Information
+	const id = req.params.id;
 	var month  = req.body.month;
 	var year = req.body.year;
 
-	// Construct Specific SQL Query
-	var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
-	var getQuery = viewOne + selectQuery;
+	var lastday = function(year,month){
+		return  new Date(year, month +1, 0).getDate();
+	}
 
-	pool.query(getQuery, [id],(err, data) => {
-		res.render('summaryInfo2', { title: 'View Summary Information', months: months, data: data.rows });
+	var lastdayofmonth = lastday(year,month);
+	var startMonth = "'" + year + "-" + month + "-" + "01 00:00:00'";
+	var endMonth = "'" +year + "-" + month + "-" + lastdayofmonth + "23:59:00'";
+
+
+	// Construct Specific SQL Query
+	//var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
+	var query_one = 'SELECT count(oid) as totalorders, sum(price) as totalcost from Orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 AND timeriderdeparts between ' + startMonth +'and' + endMonth;
+	var temptable = 'WITH FoodCount AS (SELECT count(foodname) as totalFoodOrders, foodname from Orders natural join OrderContainsFoodItems where rid=$1 AND timeriderdeparts between ' + startMonth + 'and' + endMonth + ')';
+	var query_two = 'SELECT foodname from FoodCount ORDER BY totalFoodOrders DESC limit 5)';
+	var query_combine = temptable + query_two;
+	//var query_combineOne = viewOne + selectQuery;
+	var myList = []; //myList = [month,year,totalnumOrders,totalCost,top5]
+	var top5 = "";
+
+	pool.query(query_one, [id],(err, data) => {	
+		myList.push(data);
 	});
+	pool.query(query_combine,[id],(err,data) => {
+		var i;
+		var rows;
+		if(data.rows.length < 5){
+			rows = data.rows.length;
+		} else { rows = 5;}
+
+		for (i = 0; i < rows; i++) {
+		  if(i == 4){
+			  top5 += data[i];
+		  }
+			top5 += data[i] + ", ";
+		  
+		}
+	});
+	myList.push(top5);
+
+	res.render('summaryInfo2', { title: 'View Summary Information', months: months, data: myList });
 });
 
 
