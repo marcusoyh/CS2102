35b386fbe9c84eb4bafda6081bd2dc55fa640c57
Celesse Tay <celessetay@gmail.com>
2020-04-21 15:47:50 +0800

    queries should work

--- App/routes/summaryInfo.js
+++ App/routes/summaryInfo.js
@@ -34,9 +34,9 @@ router.post('/:id', function(req, res, next) {
 
 	// Construct Specific SQL Query
 	//var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
-	var query_one = 'SELECT count(oid) as totalorders, sum(price) as totalcost from Orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 AND timeriderdeparts between ' + startMonth +'and' + endMonth;
-	var temptable = 'WITH FoodCount AS (SELECT count(foodname) as totalFoodOrders, foodname from Orders natural join OrderContainsFoodItems where rid=$1 AND timeriderdeparts between ' + startMonth + 'and' + endMonth + ')';
-	var query_two = 'SELECT foodname from FoodCount ORDER BY totalFoodOrders DESC limit 5)';
+	var query_one = 'SELECT count(oid) as totalorders, sum(totalprice) as totalcost from Orders natural join OrderContainsFoodItems where rid =$1 AND timeriderdeparts between ' + startMonth +'and' + endMonth;
+	var temptable = 'WITH FoodCount AS (SELECT count(name) as totalFoodOrders, name from Orders natural join OrderContainsFoodItems where rid=$1 AND timeriderdeparts between ' + startMonth + 'and' + endMonth + 'group by name)';
+	var query_two = 'SELECT name from FoodCount ORDER BY totalFoodOrders DESC limit 5)';
 	var query_combine = temptable + query_two;
 	//var query_combineOne = viewOne + selectQuery;
 	var myList = []; //myList = [month,year,totalnumOrders,totalCost,top5]
@@ -64,50 +64,6 @@ router.post('/:id', function(req, res, next) {
 
 	res.render('summaryInfo2', { title: 'View Summary Information', months: months, data: myList });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* GET method to generate the tables 
-router.get('/:id', function (req, res, next) {
-	const id = req.params.id;
-	var viewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1 group by month)';
-	var getQuery = viewOne + selectQuery;
-
-	pool.query(getQuery, [id],(err, data) => {
-		res.render('summaryInfo', { title: 'View Summary Information', months: months, data: data.rows });
-	});
-});*/
-
-
-/* POST method to filter table
-router.post('/:id', function (req, res, next) {
-	// Retrieve Information
-	// var dateString = req.body.monthToFilter;
-
-	var postViewOne = 'with viewone as (SELECT DATE_TRUNC(' + "'month'" + ',timeriderdeparts) AS month, count(oid) as totalorders, sum(price) as totalcost from orders natural join OrderContainsFoodItems natural join RestaurantFoodItems where rid =$1, month between ' + "'2013-1-1' and " +" '2016-1-1'" + 'group by month)';
-	
-	var postQuery = postViewOne +  selectQuery;
-	//date.isValid() is a method that returns true/false, can validate whether legit date was entered
-	pool.query(postQuery, (err, data) => {
-		res.render('summaryInfo', { title: 'Summary Info, Filtered', months: months, data: data.rows })
-	});
-
-});*/
 
 //to query for searching for specific months, we do select * from orders where timeDelivered between '2015-10-10' and '2016-10-10';
 module.exports = router;
