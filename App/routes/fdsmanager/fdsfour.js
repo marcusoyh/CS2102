aec094b6d6d2396a3685a2333a4aed230e6b000c
Celesse Tay <celessetay@gmail.com>
2020-04-30 23:07:28 +0800

    update

--- App/routes/fdsmanager/fdsfour.js
+++ App/routes/fdsmanager/fdsfour.js
@@ -21,7 +21,8 @@ router.post('/', function(req, res, next) {
 	const id = req.params.id;
 	var month  = req.body.month;
     var year = req.body.year;
-    var driver = req.body.rider;
+	var driver = req.body.rider;
+	var myList = [];
 
 	var lastday = function(year,month){
 		return  new Date(year, month +1, 0).getDate();
@@ -31,15 +32,18 @@ router.post('/', function(req, res, next) {
 	var startMonth = "'" + year + "-" + month + "-" + "01 00:00:00'";
 	var endMonth = "'" +year + "-" + month + "-" + lastdayofmonth + "23:59:00'";
 
-
     // Construct Specific SQL Query
     //total number of hours, salary
 	
-    var query_first = "SELECT name, count(oid) as totalOrders, sum(deliveryTime - timeRiderLeavesRestaurant) as totaldeliveryTime, sum(riderRating) as totalRating FROM Users natural join Drivers natural join Orders WHERE timeRiderLeavesRestaurant between " + startMonth +'and' + endMonth + " AND name = '" + driver + "' GROUP BY name";
+    var query_first = "SELECT name, count(oid) as totalOrders, sum(deliveryTime - timeRiderLeavesRestaurant) as totaldeliveryTime, sum(riderRating) as totalRating FROM Users natural join Drivers natural join Orders WHERE timeRiderLeavesRestaurant between " + startMonth + "AND" + endMonth + " AND name = '" + driver + "' GROUP BY name";
     
     pool.query(query_first, [id],(err, data) => {	
-	res.render('fdsmanager/summaryfour', { title: 'Summary Four', month:month,year:year, data: data.rows });
+		myList.push(month);
+		myList.push(year);
+		myList.push(data);
+		res.render('fdsmanager/summaryfour', { title: 'Summary Four',  data: myList });
 	});
+
 });
 
 //to query for searching for specific months, we do select * from orders where timeDelivered between '2015-10-10' and '2016-10-10';
